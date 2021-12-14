f081f6fc239ab591718fd1c22336f8b1c7ac3827
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-14 17:20:59 -0500

    make component folder and header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Konnichiwa</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
