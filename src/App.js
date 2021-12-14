7e8d374ee007ebce51e13d7943c96e35ce478dba
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-14 18:55:52 -0500

    configure eslint to ignore not importing React

--- src/App.js
+++ src/App.js
@@ -24,10 +24,16 @@ const App = () => {
       reminder: false
     }
   ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log('deleeete', id);
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 };
