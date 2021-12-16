b3b388c79e26893ddbba9db9e023a4f1b1b2cfc1
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-16 17:59:14 -0500

    implement getTasks in useEffect

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ const App = () => {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks');
-      const data = await res.json();
-      console.log('data:', data);
+    const getTasks = async () => {
+      const serverTasks = await fetchTasks();
+      setTasks(serverTasks);
     };
-    fetchTasks();
+    getTasks();
   }, []);
+
+  // seperated fetchTasks function from useEffect so it can be used elsewhere
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    console.log('data:', data);
+    return data;
+  };
 
   // Add Task
   const addTask = (task) => {
